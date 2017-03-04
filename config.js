8d5b5c2516a075d3cc54fc6f279a3f5c43b02cf0
cnnngroup <pobozkajma@gmail.com>
2017-03-04 13:37:14 +0100

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 module.exports = {
-  consumer_key:         '3RzJ8BkLdN3kH32eOn3Na91qu',
-  consumer_secret:      'YgY9M8d0tV3I2Zn4x0qyKtukLSGD5ZLvuOmqXhP3k87bbwsP4J',
-  access_token:         '836525599107723265-vyLcO7bX2FNjYSiYjcTVc7O73bjAvQL',
-  access_token_secret:  'VfV2YBrm50dZsEljLlr5D8xLbmQaHwYeBBjAwjIr6fCmJ',
+  consumer_key:         '--',
+  consumer_secret:      '--',
+  access_token:         '--',
+  access_token_secret:  '--',
 }
