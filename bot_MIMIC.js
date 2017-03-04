8ddddb374b59a584eee01ff035081b15993bbe8a
cnnngroup <pobozkajma@gmail.com>
2017-03-04 14:06:40 +0100

    Update bot_MIMIC.js

--- bot_MIMIC.js
+++ bot_MIMIC.js
@@ -1,3 +1,24 @@
+/*
+Functionality: A bot that allows a user to instantly reply back to a tweet that they were mentioned in, with the same 
+message
+
+Functions:   tweetMention() is a callback function that takes eventMsg when a tweet is populated onto a timeline. The 
+function uses regular expressions, or regex, to parse through the tweet that the user is mentioned in. If the user is 
+found, the the function removes the user's @user_name from the tweet, and invokes tweetIt(). The function tweetIt() is
+invoked with the two paramters, text(the text to send back to the user invoking the original tweet) and the tweet ID so 
+that we are able to post a reply directly to the tweet.
+
+How to run the program:
+
+1) Make sure that you've correctly configured your Twitter Keys and node, which can be done by following Daniel Shiffman's 
+first three videos in the playlist.
+
+2) Modify line 15: inserting YOUR Twitter handle, i.e. @Jack. "/@ /g" lets javascript know that you're parsing a regular 
+expression. So replace 'INSERT_YOUR_@NAME' on line 15 and 16 with your Twitter handle
+
+3) For this program, we don't have to modify the text we're going to tweet out because if the user sending it to us did 
+not exceed the 140 character limit, we won't either. So after completing steps 1 and 2, just run : node mimicBot.js
+*/
 console.log("Bot is now starting . . .");
 
 var Twit = require('twit');
